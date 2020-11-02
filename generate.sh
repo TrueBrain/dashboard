d9726ab3a46fdb673dfa2986836f0d11fd2e434b
Patric Stout <truebrain@openttd.org>
2020-11-02 11:41:49 +0100

    Add: also add a few personal repositories on the list

--- generate.sh
+++ generate.sh
@@ -15,7 +15,8 @@ py-helpers
 website
 "
 
-echo "| Name | PRs | Commits | Release | Staging | Production |" > README.md
+echo "## OpenTTD" > README.md
+echo "| Name | PRs | Commits | Release | Staging | Production |" >> README.md
 echo "| --- | --- | --- | --- | --- | --- |" >> README.md
 
 for repo in ${REPOS}; do
@@ -44,6 +45,36 @@ for repo in ${REPOS}; do
             echo -n "| [![Staging ${repo}](https://img.shields.io/github/deployments/OpenTTD/${repo}/staging?label=)](https://github.com/OpenTTD/${repo}/deployments)" >> README.md
         fi
         echo -n "| [![Production ${repo}](https://img.shields.io/github/deployments/OpenTTD/${repo}/production?label=)](https://github.com/OpenTTD/${repo}/deployments)" >> README.md
+    fi
+    echo "|" >> README.md
+done
+
+
+REPOS="
+actions-flake8
+truewiki
+wikitexthtml
+"
+
+echo "## TrueBrain" >> README.md
+echo "| Name | PRs | Commits | Release | Staging | Production |" >> README.md
+echo "| --- | --- | --- | --- | --- | --- |" >> README.md
+
+for repo in ${REPOS}; do
+    echo -n "| ${repo}" >> README.md
+    echo -n "| [![PRs ${repo}](https://img.shields.io/github/issues-pr/TrueBrain/${repo}?label=)](https://github.com/TrueBrain/${repo}/pulls)" >> README.md
+    echo -n "| [![Commits ${repo}](https://img.shields.io/github/commits-since/TrueBrain/${repo}/latest/master?label=)](https://github.com/TrueBrain/${repo}/commits/master)" >> README.md
+    if [ "${repo}" = "wikitexthtml" ]; then
+        echo -n "| [![Release Workflow ${repo}](https://img.shields.io/github/workflow/status/TrueBrain/${repo}/Release?label=)](https://github.com/TrueBrain/${repo}/actions?query=workflow%3A%22Release%22)" >> README.md
+    else
+        echo -n "|" >> README.md
+    fi
+    if [ "${repo}" = "actions-flake8" ] || [ "${repo}" = "truewiki" ] || [ "${repo}" = "wikitexthtml" ]; then
+        echo -n "|" >> README.md
+        echo -n "|" >> README.md
+    else
+        echo -n "| [![Staging ${repo}](https://img.shields.io/github/deployments/TrueBrain/${repo}/staging?label=)](https://github.com/TrueBrain/${repo}/deployments)" >> README.md
+        echo -n "| [![Production ${repo}](https://img.shields.io/github/deployments/TrueBrain/${repo}/production?label=)](https://github.com/TrueBrain/${repo}/deployments)" >> README.md
     fi
     echo "|" >> README.md
 done
