48fd4317509941fc55fcbbd594e0372242de1abb
Patric Stout <truebrain@openttd.org>
2020-11-02 11:36:12 +0100

    Add: show the special "openttd-github" branch of eints

--- generate.sh
+++ generate.sh
@@ -21,7 +21,12 @@ echo "| --- | --- | --- | --- | --- | --- |" >> README.md
 for repo in ${REPOS}; do
     echo -n "| ${repo}" >> README.md
     echo -n "| [![PRs ${repo}](https://img.shields.io/github/issues-pr/OpenTTD/${repo}?label=)](https://github.com/OpenTTD/${repo}/pulls)" >> README.md
-    echo -n "| [![Commits ${repo}](https://img.shields.io/github/commits-since/OpenTTD/${repo}/latest/master?label=)](https://github.com/OpenTTD/${repo}/commits/master)" >> README.md
+    if [ "${repo}" = "eints" ]; then
+        echo -n "| [![Commits ${repo}](https://img.shields.io/github/commits-since/OpenTTD/${repo}/latest/master?label=)](https://github.com/OpenTTD/${repo}/commits/master)" >> README.md
+        echo -n " / [![Commits ${repo}](https://img.shields.io/github/commits-since/OpenTTD/${repo}/latest/openttd-github?label=)](https://github.com/OpenTTD/${repo}/commits/openttd-github)" >> README.md
+    else
+        echo -n "| [![Commits ${repo}](https://img.shields.io/github/commits-since/OpenTTD/${repo}/latest/master?label=)](https://github.com/OpenTTD/${repo}/commits/master)" >> README.md
+    fi
     if [ "${repo}" = "nml" ] || [ "${repo}" = "py-helpers" ]; then
         echo -n "| [![Release Workflow ${repo}](https://img.shields.io/github/workflow/status/OpenTTD/${repo}/Release?label=)](https://github.com/OpenTTD/${repo}/actions?query=workflow%3A%22Release%22)" >> README.md
     elif [ "${repo}" = "actions" ] || [ "${repo}" = "OpenTTD" ]; then
